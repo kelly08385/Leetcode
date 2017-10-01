cf2c74f3b2316b230c52747ef9659388ff1502dd
kelly <kelly08385@gmail.com>
2017-10-01 15:10:54 +0800

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by 林凱君 on 2017/10/1.
+//  Copyright © 2017年 林凱君. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
